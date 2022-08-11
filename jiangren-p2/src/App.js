fdee1c1f870906898f1f575ce3c54bd816439870
Eason Ma <a705193483@gmail.com>
2022-08-11 23:29:56 +0930

    Initial Commit

---
+++ jiangren-p2/src/App.js
@@ -0,0 +1,11 @@
+import WeatherAPITable from "./components/weatherAPITable";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherAPITable />
+    </div>
+  );
+}
+
+export default App;
---
+++ jiangren-p2/src/components/weatherAPITable/weatherAPITable.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './weatherAPITable.css';
+
+const WeatherAPITable = () => {
+    return (
+        <h1 className="weatherApiHeading">Weather API</h1>
+    );
+}
+
+export default WeatherAPITable;
